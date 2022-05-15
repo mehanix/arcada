a645bf52840ea111c7f15e6329c0080e053313b4
Nicoleta Ciausu <nixikunn@gmail.com>
2022-05-15 17:39:56 +0300

    layour edits

--- src/stores/ToolStore.tsx
+++ src/stores/ToolStore.tsx
@@ -22,6 +22,7 @@ export enum Tool {
     FurnitureEdit,
     FurnitureRemove,
     FurnitureAddWindow,
+    FurnitureAddDoor,
     View
 };
 
--- src/ui/FurnitureControls/FurnitureTools.tsx
+++ src/ui/FurnitureControls/FurnitureTools.tsx
@@ -1,50 +1,96 @@
-import { Group, Button, createStyles } from "@mantine/core";
+import { Center, Group, Tooltip } from "@mantine/core";
+import { useState } from "react";
 import { Tool, useStore } from "../../stores/ToolStore";
+import { SegmentedControl } from '@mantine/core';
+import { Door, Edit, Eraser, Plus, Window } from "tabler-icons-react";
 
-const useStyles = createStyles((theme) => ({
-  button: {
-    borderRadius: 0,
+// const useStyles = createStyles((theme) => ({
+//   button: {
+//     borderRadius: 0,
 
-    '&:not(:first-of-type)': {
-      borderLeftWidth: 0,
-    },
+//     '&:not(:first-of-type)': {
+//       borderLeftWidth: 0,
+//     },
 
-    '&:first-of-type': {
-      borderTopLeftRadius: theme.radius.sm,
-      borderBottomLeftRadius: theme.radius.sm,
-    },
+//     '&:first-of-type': {
+//       borderTopLeftRadius: theme.radius.sm,
+//       borderBottomLeftRadius: theme.radius.sm,
+//     },
 
-    '&:last-of-type': {
-      borderTopRightRadius: theme.radius.sm,
-      borderBottomRightRadius: theme.radius.sm,
-    },
-  },
-}));
+//     '&:last-of-type': {
+//       borderTopRightRadius: theme.radius.sm,
+//       borderBottomRightRadius: theme.radius.sm,
+//     },
+//   },
+// }));
 
 const tools = [
-  { label: 'Edit', tool:Tool.FurnitureEdit },
-  { label: 'Remove', tool:Tool.FurnitureRemove },
-  {label: 'Add window', tool:Tool.FurnitureAddWindow }
+  {
+    label: (
+      <Tooltip label={"Edit furniture mode"} position="bottom" withArrow transitionDuration={0}>
+      <Center>
+        <Edit size={20} />
+      </Center>
+      </Tooltip>
+
+    ), value: Tool.FurnitureEdit.toString()
+  },
+  { label: (
+    <Tooltip label={"Delete furniture mode"} position="bottom" withArrow transitionDuration={0}>
+
+    <Center>
+      <Eraser size={20} />
+    </Center>
+    </Tooltip>
+  ), value: Tool.FurnitureRemove.toString() },
+  { label: (
+    <Tooltip label={"Add window"} position="bottom" withArrow transitionDuration={0}>
+
+    <Center>
+      <Window size={20} />
+      <Plus size={12} />
+    </Center>
+    </Tooltip>
+  ), value: Tool.FurnitureAddWindow.toString() },
+  { label: (
+    <Tooltip label={"Add door"} position="bottom" withArrow transitionDuration={0}>
+    <Center>
+      <Door size={20} />
+      <Plus size={12} />
+    </Center>
+    </Tooltip>
+  ), value: Tool.FurnitureAddDoor.toString() }
 ];
 
 export function FurnitureTools() {
-  const { classes } = useStyles();
+  // const { classes } = useStyles();
   const { setTool } = useStore()
+  const [value, setValue] = useState(Tool.FurnitureEdit.toString());
 
-  const toolButtons = tools.map((link, _) => (
-    <Button variant="default" className={classes.button}
-      {...link}
-      key={link.label}
-      onClick={() => {
-        setTool( link.tool )
-      }
-      }
-    > {link.label}</Button>
-  ));
+  // const toolButtons = tools.map((link, _) => (
+  //   <Button variant="default" className={classes.button}
+  //     {...link}
+  //     key={link.label}
+  //     onClick={() => {
+  //       setTool( link.tool )
+  //     }
+  //     }
+  //   > {link.label}</Button>
+  // ));
+  const control = <SegmentedControl
+    value={value}
+    onChange={(value) => {
+      setValue(value)
+      console.log(Tool[parseInt(value)])
+      let tool: Tool = parseInt(value);
+      setTool(tool)
+    }}
+    data={tools}
+  />
 
   return (
     <Group grow spacing={0}>
-      {toolButtons}
+      {control}
     </Group>
   );
 }
